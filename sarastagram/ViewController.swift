0558ae5e877bdc9f3381a3a091e7cea6198d1c8d
向久保更 <mukaikubo.sara@mukaihisayasunoMacBook-Air.local>
2017-09-17 15:32:21 +0900

    Initial Commit

---
+++ sarastagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sarastagram
+//
+//  Created by 向久保 更 on 2017/09/17.
+//  Copyright © 2017年 向久保 更. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
